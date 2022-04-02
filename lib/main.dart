6f3cd1647410c0089aebc6907ac62ccdd4ff4fee
takumi <takumim1005@gmail.com>
2022-04-02 11:09:46 +0900

    rewritten to ConsumerStatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -25,45 +25,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) => Text(
-            ref.watch(titleProvider),
-          ),
+        title: Text(
+          ref.watch(titleProvider),
         ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(messageProvider),
-              ),
+            Text(
+              ref.watch(messageProvider),
             ),
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(countProvider).toString(),
-                style: Theme.of(context).textTheme.headline4,
-              ),
+            Text(
+              ref.watch(countProvider).toString(),
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-      floatingActionButton: Consumer(
-        builder: (context, ref, child) {
-          return FloatingActionButton(
-            onPressed: () => ref.read(countProvider.state).state++,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          );
-        },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(countProvider.state).state++,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
