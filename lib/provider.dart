f98b12aacec9fe357eeb3e0e88c390f9d558a493
takumi <takumim1005@gmail.com>
2022-04-02 10:22:08 +0900

    create stateProvider

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final titleProvider = Provider<String>((ref) {
-  return 'Riverpod Demo Home Page';
-});
+final titleProvider = Provider<String>((ref) => 'Riverpod Demo Home Page');
 
 final messageProvider = Provider<String>((ref) => 'You have pushed');
+
+final countProvider = StateProvider<int>((ref) => 0);
