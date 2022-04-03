b040ff50411708963cbb7d0b991eb45ca660c814
takumi <takumim1005@gmail.com>
2022-04-03 22:10:42 +0900

    Make the data class a stateProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/provider.dart';
+import 'data/count_data.dart';
 
 void main() {
   runApp(
@@ -50,31 +51,58 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(messageProvider),
             ),
             Text(
-              ref.watch(countProvider).toString(),
+              ref.watch(countDataProvider).count.toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 FloatingActionButton(
-                  onPressed: () => ref.read(countProvider.state).state++,
+                  onPressed: () {
+                    CountData countData = ref.read(countDataProvider);
+                    ref.read(countDataProvider.state).state =
+                        countData.copyWith(
+                      count: countData.count + 1,
+                      countUp: countData.countUp + 1,
+                    );
+                  },
                   child: const Icon(CupertinoIcons.plus),
                 ),
                 FloatingActionButton(
-                  onPressed: () => ref.read(countProvider.state).state++,
+                  onPressed: () {
+                    CountData countData = ref.read(countDataProvider);
+                    ref.read(countDataProvider.state).state =
+                        countData.copyWith(
+                      count: countData.count - 1,
+                      countDown: countData.countDown + 1,
+                    );
+                  },
                   child: const Icon(CupertinoIcons.minus),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [Text('1'), Text('2')],
+              children: const [
+                Text(
+                  ref.watch(countDataProvider).countUp.toString(),
+                ),
+                Text(
+                  ref.watch(countDataProvider).countDown.toString(),
+                )
+              ],
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(countProvider.state).state++,
+        onPressed: () {
+          ref.read(countDataProvider.state).state = CountData(
+            count: 0,
+            countUp: 0,
+            countDown: 0,
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.refresh),
       ),
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/data/count_data.dart';
 
 final titleProvider = Provider<String>((ref) => 'Riverpod Demo Home Page');
 
 final messageProvider = Provider<String>((ref) => 'You have pushed');
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>((ref) => const CountData(
+      count: 0,
+      countUp: 0,
+      countDown: 0,
+    ));
