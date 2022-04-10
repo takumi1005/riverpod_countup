db2fcb3dcfc4fe2cae5418c746726916ea4d0861
takumi <takumim1005@gmail.com>
2022-04-10 17:28:21 +0900

    replace method

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/provider.dart';
 import 'package:riverpod_countup/view_model.dart';
 
-import 'data/count_data.dart';
-
 void main() {
   runApp(
     const ProviderScope(
@@ -69,25 +67,11 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 FloatingActionButton(
-                  onPressed: () {
-                    CountData countData = ref.read(countDataProvider);
-                    ref.read(countDataProvider.state).state =
-                        countData.copyWith(
-                      count: countData.count + 1,
-                      countUp: countData.countUp + 1,
-                    );
-                  },
+                  onPressed: _viewModel.onIncrease,
                   child: const Icon(CupertinoIcons.plus),
                 ),
                 FloatingActionButton(
-                  onPressed: () {
-                    CountData countData = ref.read(countDataProvider);
-                    ref.read(countDataProvider.state).state =
-                        countData.copyWith(
-                      count: countData.count - 1,
-                      countDown: countData.countDown + 1,
-                    );
-                  },
+                  onPressed: _viewModel.onDecrease,
                   child: const Icon(CupertinoIcons.minus),
                 ),
               ],
@@ -107,13 +91,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(countDataProvider.state).state = const CountData(
-            count: 0,
-            countUp: 0,
-            countDown: 0,
-          );
-        },
+        onPressed: _viewModel.onReset,
         tooltip: 'Increment',
         child: const Icon(Icons.refresh),
       ),
--- lib/view_model.dart
+++ lib/view_model.dart
@@ -12,10 +12,25 @@ class ViewModel {
 
   get count => _ref.watch(countDataProvider).count.toString();
 
-  get countUp =>
-      _ref.watch(countDataProvider.select((value) => value.countUp)).toString();
+  get countUp => _ref.watch(countDataProvider.select((value) => value.countUp)).toString();
 
-  get countDown => _ref
-      .watch(countDataProvider.select((value) => value.countDown))
-      .toString();
+  get countDown => _ref.watch(countDataProvider.select((value) => value.countDown)).toString();
+
+  void onIncrease() {
+    _logic.increase();
+
+    _ref.watch(countDataProvider.state).state = _logic.countData;
+  }
+
+  void onDecrease() {
+    _logic.decrease();
+
+    _ref.watch(countDataProvider.state).state = _logic.countData;
+  }
+
+  void onReset() {
+    _logic.reset();
+
+    _ref.watch(countDataProvider.state).state = _logic.countData;
+  }
 }
